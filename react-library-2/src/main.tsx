1dc01d2822846c8c3badfabc458bf429c57642ca
Fisnik <ukaj808@gmail.com>
2023-07-31 11:02:27 +0200

    recreated issue

--- react-library-2/src/main.tsx
+++ react-library-2/src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export const createRoot = (id: string) => {
+  ReactDOM.createRoot(document.getElementById(id)!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+};
--- react-library-2/vite.config.ts
+++ react-library-2/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      formats: ['es'],
+      entry: 'src/main.tsx',
+    },
+  },
 })
